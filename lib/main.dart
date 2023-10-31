f76438d571f29869022548327c7d8a0c0aca6085
Snowball_233 <wangrb.qq@outlook.com>
2023-11-01 00:37:41 +0800

    test

--- lib/main.dart
+++ lib/main.dart
@@ -108,9 +108,11 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Container(
+              child: Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
             ),
           ],
         ),
